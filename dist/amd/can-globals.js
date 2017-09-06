f4b4462a9f56320fe96b64cb84613f5ba17ee621
imaustink <austin@austinkurpuis.com>
2017-09-06 09:25:01 -0700

    0.0.6

---
+++ dist/amd/can-globals.js
@@ -0,0 +1,23 @@
+/*can-globals@0.0.5#can-globals*/
+define([
+    'require',
+    'exports',
+    'module',
+    './can-globals-instance',
+    './global/global',
+    './document/document',
+    './location/location',
+    './mutation-observer/mutation-observer'
+], function (require, exports, module) {
+    (function (global) {
+        'use strict';
+        var globals = require('./can-globals-instance');
+        require('./global/global');
+        require('./document/document');
+        require('./location/location');
+        require('./mutation-observer/mutation-observer');
+        module.exports = globals;
+    }(function () {
+        return this;
+    }()));
+});
---
+++ dist/amd/document/document.js
@@ -0,0 +1,20 @@
+/*can-globals@0.0.5#document/document*/
+define([
+    'require',
+    'exports',
+    'module',
+    '../global/global',
+    '../can-globals-instance'
+], function (require, exports, module) {
+    (function (global) {
+        'use strict';
+        require('../global/global');
+        var globals = require('../can-globals-instance');
+        globals.define('document', function () {
+            return globals.getKeyValue('global').document;
+        });
+        module.exports = globals.makeExport('document');
+    }(function () {
+        return this;
+    }()));
+});
