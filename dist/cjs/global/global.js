c50bd7209f3a063d716f5016e7f43b3e2432127e
imasustink <austin@austinkurpuis.com>
2017-09-22 16:57:49 -0700

    0.2.1

---
+++ dist/cjs/global/global.js
@@ -0,0 +1,7 @@
+/*can-globals@0.2.0#global/global*/
+'use strict';
+var globals = require('../can-globals-instance.js');
+globals.define('global', function () {
+    return typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScope ? self : typeof process === 'object' && {}.toString.call(process) === '[object process]' ? global : window;
+});
+module.exports = globals.makeExport('global');
---
+++ dist/cjs/mutation-observer/mutation-observer.js
@@ -0,0 +1,9 @@
+/*can-globals@0.2.0#mutation-observer/mutation-observer*/
+'use strict';
+require('../global/global.js');
+var globals = require('../can-globals-instance.js');
+globals.define('MutationObserver', function () {
+    var GLOBAL = globals.getKeyValue('global');
+    return GLOBAL.MutationObserver || GLOBAL.WebKitMutationObserver || GLOBAL.MozMutationObserver;
+});
+module.exports = globals.makeExport('MutationObserver');
