563eb26ce6b865721cb5a89332b87c9c839d985d
imasustink <austin@austinkurpuis.com>
2017-10-02 10:59:13 -0700

    0.2.3

---
+++ dist/amd/is-browser-window/is-browser-window.js
@@ -0,0 +1,18 @@
+/*can-globals@0.2.2#is-browser-window/is-browser-window*/
+define([
+    'require',
+    'exports',
+    'module',
+    '../can-globals-instance'
+], function (require, exports, module) {
+    (function (global) {
+        'use strict';
+        var globals = require('../can-globals-instance');
+        globals.define('isBrowserWindow', function () {
+            return typeof window !== 'undefined' && typeof document !== 'undefined' && typeof SimpleDOM === 'undefined';
+        });
+        module.exports = globals.makeExport('isBrowserWindow');
+    }(function () {
+        return this;
+    }()));
+});
---
+++ dist/cjs/can-globals-instance.js
@@ -0,0 +1,11 @@
+/*can-globals@0.2.2#can-globals-instance*/
+'use strict';
+var namespace = require('can-namespace');
+var Globals = require('./can-globals-proto.js');
+var globals = new Globals();
+if (namespace.globals) {
+    throw new Error('You can\'t have two versions of can-globals, check your dependencies');
+} else {
+    module.exports = namespace.globals = globals;
+}
+module.exports = globals;
