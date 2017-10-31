e12013217e1541216ec120741ca19896597a2436
imasustink <austin@austinkurpuis.com>
2017-10-30 19:24:11 -0700

    0.2.4

---
+++ dist/global/can-globals.js
@@ -0,0 +1,410 @@
+/*[global-shim-start]*/
+(function(exports, global, doEval) {
+	// jshint ignore:line
+	var origDefine = global.define;
+
+	var get = function(name) {
+		var parts = name.split("."),
+			cur = global,
+			i;
+		for (i = 0; i < parts.length; i++) {
+			if (!cur) {
+				break;
+			}
+			cur = cur[parts[i]];
+		}
+		return cur;
+	};
+	var set = function(name, val) {
+		var parts = name.split("."),
+			cur = global,
+			i,
+			part,
+			next;
+		for (i = 0; i < parts.length - 1; i++) {
+			part = parts[i];
+			next = cur[part];
+			if (!next) {
+				next = cur[part] = {};
+			}
+			cur = next;
+		}
+		part = parts[parts.length - 1];
+		cur[part] = val;
+	};
+	var useDefault = function(mod) {
+		if (!mod || !mod.__esModule) return false;
+		var esProps = { __esModule: true, default: true };
+		for (var p in mod) {
+			if (!esProps[p]) return false;
+		}
+		return true;
+	};
+
+	var hasCjsDependencies = function(deps) {
+		return (
+			deps[0] === "require" && deps[1] === "exports" && deps[2] === "module"
+		);
+	};
+
+	var modules =
+		(global.define && global.define.modules) ||
+		(global._define && global._define.modules) ||
+		{};
+	var ourDefine = (global.define = function(moduleName, deps, callback) {
+		var module;
+		if (typeof deps === "function") {
+			callback = deps;
+			deps = [];
+		}
+		var args = [],
+			i;
+		for (i = 0; i < deps.length; i++) {
+			args.push(
+				exports[deps[i]]
+					? get(exports[deps[i]])
+					: modules[deps[i]] || get(deps[i])
+			);
+		}
+		// CJS has no dependencies but 3 callback arguments
+		if (hasCjsDependencies(deps) || (!deps.length && callback.length)) {
+			module = { exports: {} };
+			args[0] = function(name) {
+				return exports[name] ? get(exports[name]) : modules[name];
+			};
+			args[1] = module.exports;
+			args[2] = module;
+		} else if (!args[0] && deps[0] === "exports") {
+			// Babel uses the exports and module object.
+			module = { exports: {} };
+			args[0] = module.exports;
+			if (deps[1] === "module") {
+				args[1] = module;
+			}
+		} else if (!args[0] && deps[0] === "module") {
+			args[0] = { id: moduleName };
+		}
+
+		global.define = origDefine;
+		var result = callback ? callback.apply(null, args) : undefined;
+		global.define = ourDefine;
+
+		// Favor CJS module.exports over the return value
+		result = module && module.exports ? module.exports : result;
+		modules[moduleName] = result;
+
+		// Set global exports
+		var globalExport = exports[moduleName];
+		if (globalExport && !get(globalExport)) {
+			if (useDefault(result)) {
+				result = result["default"];
+			}
+			set(globalExport, result);
+		}
+	});
+	global.define.orig = origDefine;
+	global.define.modules = modules;
+	global.define.amd = true;
+	ourDefine("@loader", [], function() {
+		// shim for @@global-helpers
+		var noop = function() {};
+		return {
+			get: function() {
+				return { prepareGlobal: noop, retrieveGlobal: noop };
+			},
+			global: global,
+			__exec: function(__load) {
+				doEval(__load.source, global);
+			}
+		};
+	});
+})(
+	{},
+	typeof self == "object" && self.Object == Object ? self : window,
+	function(__$source__, __$global__) {
+		// jshint ignore:line
+		eval("(function() { " + __$source__ + " \n }).call(__$global__);");
+	}
+);
+
+/*can-globals@0.2.3#can-globals-proto*/
+define('can-globals/can-globals-proto', [
+    'require',
+    'exports',
+    'module',
+    'can-reflect'
+], function (require, exports, module) {
+    (function (global) {
+        'use strict';
+        var canReflect = require('can-reflect');
+        function dispatch(key, value) {
+            var handlers = this.eventHandlers[key];
+            if (handlers) {
+                var handlersCopy = handlers.slice();
+                for (var i = 0; i < handlersCopy.length; i++) {
+                    handlersCopy[i](value);
+                }
+            }
+        }
+        function Globals() {
+            this.eventHandlers = {};
+            this.properties = {};
+        }
+        Globals.prototype.define = function (key, value, enableCache) {
+            if (enableCache === undefined) {
+                enableCache = true;
+            }
+            if (!this.properties[key]) {
+                this.properties[key] = {
+                    default: value,
+                    value: value,
+                    enableCache: enableCache
+                };
+            }
+            return this;
+        };
+        Globals.prototype.getKeyValue = function (key) {
+            var property = this.properties[key];
+            if (property) {
+                if (typeof property.value === 'function') {
+                    if (property.cachedValue) {
+                        return property.cachedValue;
+                    }
+                    if (property.enableCache) {
+                        property.cachedValue = property.value();
+                        return property.cachedValue;
+                    } else {
+                        return property.value();
+                    }
+                }
+                return property.value;
+            }
+        };
+        Globals.prototype.makeExport = function (key) {
+            return function (value) {
+                if (arguments.length === 0) {
+                    return this.getKeyValue(key);
+                }
+                if (typeof value === 'undefined' || value === null) {
+                    this.deleteKeyValue(key);
+                } else {
+                    if (typeof value === 'function') {
+                        this.setKeyValue(key, function () {
+                            return value;
+                        });
+                    } else {
+                        this.setKeyValue(key, value);
+                    }
+                    return value;
+                }
+            }.bind(this);
+        };
+        Globals.prototype.offKeyValue = function (key, handler) {
+            if (this.properties[key]) {
+                var handlers = this.eventHandlers[key];
+                if (handlers) {
+                    var i = handlers.indexOf(handler);
+                    handlers.splice(i, 1);
+                }
+            }
+            return this;
+        };
+        Globals.prototype.onKeyValue = function (key, handler) {
+            if (this.properties[key]) {
+                if (!this.eventHandlers[key]) {
+                    this.eventHandlers[key] = [];
+                }
+                this.eventHandlers[key].push(handler);
+            }
+            return this;
+        };
+        Globals.prototype.deleteKeyValue = function (key) {
+            var property = this.properties[key];
+            if (property !== undefined) {
+                property.value = property.default;
+                property.cachedValue = undefined;
+                dispatch.call(this, key, property.value);
+            }
+            return this;
+        };
+        Globals.prototype.setKeyValue = function (key, value) {
+            if (!this.properties[key]) {
+                return this.define(key, value);
+            }
+            var property = this.properties[key];
+            property.value = value;
+            property.cachedValue = undefined;
+            dispatch.call(this, key, value);
+            return this;
+        };
+        Globals.prototype.reset = function () {
+            for (var key in this.properties) {
+                if (this.properties.hasOwnProperty(key)) {
+                    this.properties[key].cachedValue = undefined;
+                    dispatch.call(this, key, this.getKeyValue(key));
+                }
+            }
+            return this;
+        };
+        canReflect.assignSymbols(Globals.prototype, {
+            'can.getKeyValue': Globals.prototype.getKeyValue,
+            'can.setKeyValue': Globals.prototype.setKeyValue,
+            'can.deleteKeyValue': Globals.prototype.deleteKeyValue,
+            'can.onKeyValue': Globals.prototype.onKeyValue,
+            'can.offKeyValue': Globals.prototype.offKeyValue
+        });
+        module.exports = Globals;
+    }(function () {
+        return this;
+    }()));
+});
+/*can-globals@0.2.3#can-globals-instance*/
+define('can-globals/can-globals-instance', [
+    'require',
+    'exports',
+    'module',
+    'can-namespace',
+    'can-globals/can-globals-proto'
+], function (require, exports, module) {
+    (function (global) {
+        'use strict';
+        var namespace = require('can-namespace');
+        var Globals = require('can-globals/can-globals-proto');
+        var globals = new Globals();
+        if (namespace.globals) {
+            throw new Error('You can\'t have two versions of can-globals, check your dependencies');
+        } else {
+            module.exports = namespace.globals = globals;
+        }
+    }(function () {
+        return this;
+    }()));
+});
+/*can-globals@0.2.3#global/global*/
+define('can-globals/global/global', [
+    'require',
+    'exports',
+    'module',
+    'can-globals/can-globals-instance'
+], function (require, exports, module) {
+    (function (global) {
+        'use strict';
+        var globals = require('can-globals/can-globals-instance');
+        globals.define('global', function () {
+            return typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScope ? self : typeof process === 'object' && {}.toString.call(process) === '[object process]' ? global : window;
+        });
+        module.exports = globals.makeExport('global');
+    }(function () {
+        return this;
+    }()));
+});
+/*can-globals@0.2.3#document/document*/
+define('can-globals/document/document', [
+    'require',
+    'exports',
+    'module',
+    'can-globals/global/global',
+    'can-globals/can-globals-instance'
+], function (require, exports, module) {
+    (function (global) {
+        'use strict';
+        require('can-globals/global/global');
+        var globals = require('can-globals/can-globals-instance');
+        globals.define('document', function () {
+            return globals.getKeyValue('global').document;
+        });
+        module.exports = globals.makeExport('document');
+    }(function () {
+        return this;
+    }()));
+});
+/*can-globals@0.2.3#location/location*/
+define('can-globals/location/location', [
+    'require',
+    'exports',
+    'module',
+    'can-globals/global/global',
+    'can-globals/can-globals-instance'
+], function (require, exports, module) {
+    (function (global) {
+        'use strict';
+        require('can-globals/global/global');
+        var globals = require('can-globals/can-globals-instance');
+        globals.define('location', function () {
+            return globals.getKeyValue('global').location;
+        });
+        module.exports = globals.makeExport('location');
+    }(function () {
+        return this;
+    }()));
+});
+/*can-globals@0.2.3#mutation-observer/mutation-observer*/
+define('can-globals/mutation-observer/mutation-observer', [
+    'require',
+    'exports',
+    'module',
+    'can-globals/global/global',
+    'can-globals/can-globals-instance'
+], function (require, exports, module) {
+    (function (global) {
+        'use strict';
+        require('can-globals/global/global');
+        var globals = require('can-globals/can-globals-instance');
+        globals.define('MutationObserver', function () {
+            var GLOBAL = globals.getKeyValue('global');
+            return GLOBAL.MutationObserver || GLOBAL.WebKitMutationObserver || GLOBAL.MozMutationObserver;
+        });
+        module.exports = globals.makeExport('MutationObserver');
+    }(function () {
+        return this;
+    }()));
+});
+/*can-globals@0.2.3#is-browser-window/is-browser-window*/
+define('can-globals/is-browser-window/is-browser-window', [
+    'require',
+    'exports',
+    'module',
+    'can-globals/can-globals-instance'
+], function (require, exports, module) {
+    (function (global) {
+        'use strict';
+        var globals = require('can-globals/can-globals-instance');
+        globals.define('isBrowserWindow', function () {
+            return typeof window !== 'undefined' && typeof document !== 'undefined' && typeof SimpleDOM === 'undefined';
+        });
+        module.exports = globals.makeExport('isBrowserWindow');
+    }(function () {
+        return this;
+    }()));
+});
+/*can-globals@0.2.3#can-globals*/
+define('can-globals', [
+    'require',
+    'exports',
+    'module',
+    'can-globals/can-globals-instance',
+    'can-globals/global/global',
+    'can-globals/document/document',
+    'can-globals/location/location',
+    'can-globals/mutation-observer/mutation-observer',
+    'can-globals/is-browser-window/is-browser-window'
+], function (require, exports, module) {
+    (function (global) {
+        'use strict';
+        var globals = require('can-globals/can-globals-instance');
+        require('can-globals/global/global');
+        require('can-globals/document/document');
+        require('can-globals/location/location');
+        require('can-globals/mutation-observer/mutation-observer');
+        require('can-globals/is-browser-window/is-browser-window');
+        module.exports = globals;
+    }(function () {
+        return this;
+    }()));
+});
+/*[global-shim-end]*/
+(function(global) { // jshint ignore:line
+	global._define = global.define;
+	global.define = global.define.orig;
+}
+)(typeof self == "object" && self.Object == Object ? self : window);
