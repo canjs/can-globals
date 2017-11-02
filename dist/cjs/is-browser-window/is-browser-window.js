74b2b13781f8aa5904c6b732c964c7023920bd1c
imasustink <austin@austinkurpuis.com>
2017-11-02 08:45:11 -0700

    0.2.5

---
+++ dist/cjs/is-browser-window/is-browser-window.js
@@ -0,0 +1,7 @@
+/*can-globals@0.2.4#is-browser-window/is-browser-window*/
+'use strict';
+var globals = require('../can-globals-instance.js');
+globals.define('isBrowserWindow', function () {
+    return typeof window !== 'undefined' && typeof document !== 'undefined' && typeof SimpleDOM === 'undefined';
+});
+module.exports = globals.makeExport('isBrowserWindow');
