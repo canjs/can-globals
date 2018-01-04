3ccf5583e6be6387da51099e37465ba691a15753
Justin Meyer <justinbmeyer@gmail.com>
2018-01-04 09:38:38 -0600

    adds isWebWorker

--- test.js
+++ test.js
@@ -3,4 +3,5 @@ require('./can-globals-test');
 require('./global/global-test');
 require('./location/location-test');
 require('./is-browser-window/is-browser-window-test');
+require('./is-web-worker/is-web-worker-test');
 require('./is-node/is-node-test');
