55f85cd50ef6c95e1d671cef0a9b4c99e88aebc8
Justin Meyer <justinbmeyer@gmail.com>
2018-05-25 22:09:49 -0500

    adds base-url

--- test.js
+++ test.js
@@ -5,3 +5,4 @@ require('./location/location-test');
 require('./is-browser-window/is-browser-window-test');
 require('./is-web-worker/is-web-worker-test');
 require('./is-node/is-node-test');
+require('./base-url/base-url-test');
